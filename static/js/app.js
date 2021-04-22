cdfc8bec5853a509b83b6a9a42e83e7faadd8abe
Kristin Flores <kristin@Joan-Steppenbeck.local>
2021-04-21 22:55:45 -0500

    clean up bar chart code

--- static/js/app.js
+++ static/js/app.js
@@ -1,101 +1,44 @@
 //// READ IN JSON ////
 
 d3.json("./data/samples.json").then(function(data) {
-  var data = bellyData  
   console.log(data);
 });
 
-// Populate drop drown list for personal IDs
-// idList = bellyData.names;
-// var selection = "";
-// for (var i = 0; i < idList; i++) {
-//     option += '<option value="'+ month[i] + '">' + month[i] + "
-//     </option>"
-
-// document.getElementById('idList').innerHTML = option
-//     select = d3.select("#selDataset");
-//     select.append("option").text(idList[i]);
-// }
+const bellyData = d3.json("./data/samples.json");
+console.log("All Data:", bellyData);
 
 
-// function updatePage() {
-//   var dropdownMenu = d3.select("selDataset");
-//   var dataset = drowndownMenu.property("value");
-// }
+//// POPULATE DROPDOWN ////
+
+//pending
 
 //// BUILD BAR CHART ////
 
 function buildSampleDataPlot() {
-    d3.json("data/samples.json").then(function(data) {
+    d3.json("./data/samples.json").then(function(barData) {
         
       // Get data from json object to build plot
-        // var id = data.samples.id;
-        var otu_ids = data.samples.otu_ids;
-        var sample_values = data.sample_values;
-        var lables = data.samples;
+        var otu_ids = barData.samples.otu_ids;
+        var sample_values = barData.sample_values;
+        var lables = barData.samples;
 
-        var trace1 = {
+        var bargraph = {
             type: "bar",
             x: sample_values,
             y: otu_ids,
             text: lables
         };
 
-        var data = [trace1];
+        var data = [bargraph];
 
         var layout = {
             title: "Top 10 Operational Taxonomic Units",
         };
 
-        Plotly.newPlot("bar",  data);
+        Plotly.newPlot("bar",  data, layout);
 
     });
 
 }
 
 buildSampleDataPlot();
-
-// var selectedID = unpack(data.samples, 0)
-
-
-// // Create default bar chart
-// function init() {
-//     var data = [{
-//         x: [],
-//         y: [], 
-//         type: "bar"
-//     }]
-
-//     Plotly.newPlot("bar", data);
-// }
-
-// // Call updatePlot when a change takes place
-// d3.selectAll('#selDataset').on("change", updatePlot);
-
-// function updatePlot() {
-//     var dropdownMenu = d3.select('#selDataset');
-//     var dataset = dropdownMenu.property("value");
-// }
-//     // Display top 10 OTU in indivdual 
-//     // Top 10 is first 10 since values are ordered large to small
-
-//     var trace1 = {
-//         x: Object.values(data.sample_values),
-//         y: Object.values(data.otu_ids),
-//         type: 'bar',
-//         boxpoints: "all"
-//         // hover: otu_labels
-//     };
-
-//     //Create data array for plot
-//     var data = [trace1],
-
-//     //Plot layout
-//     var layout = {
-//         title: "XXX",
-//         xaxis: { title: "Number" },
-//         yaxis: { title: "Type" },
-//     };
-
-//     Plotly.newPlot("plot", barChart, layout);
-// });
